a8d8ec3b9b53c15086c9708e1f107d7a90010c40
Wilson Bonato <wilsonbonatojr@gmail.com>
2024-05-26 17:41:49 -0300

    Terminei a segunda página e adicionei uma informação se está aberto ou fechado, dependendo do horário atual

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+const data = new Date()
+const horarioAtual = data.getHours() 
+const diaAtual = data.getDay()
+
+document.querySelector(".abertoFechado").innerHTML = (horarioAtual >= 18 && horarioAtual < 23 && diaAtual != 1) ? "Aberto" : "Fechado"
+document.querySelector(".divAbertoFechado").classList.add((horarioAtual >= 18 && horarioAtual < 23 && diaAtual != 1) ? "bg-green-700" : "bg-red-700")
+
 function comprar() {
     window.alert("Ainda não é possível comprar pelo site!\nUse apenas como cardápio")    
 }
