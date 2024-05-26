2cd86e5fd37588caf47286fa60106084caf4d73b
Wilson Bonato <wilsonbonatojr@gmail.com>
2024-05-26 18:24:45 -0300

    Adicionado página de contato

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,24 @@ const data = new Date()
 const horarioAtual = data.getHours() 
 const diaAtual = data.getDay()
 
-document.querySelector(".abertoFechado").innerHTML = (horarioAtual >= 18 && horarioAtual < 23 && diaAtual != 1) ? "Aberto" : "Fechado"
-document.querySelector(".divAbertoFechado").classList.add((horarioAtual >= 18 && horarioAtual < 23 && diaAtual != 1) ? "bg-green-700" : "bg-red-700")
+let abertoFechado = document.querySelector(".abertoFechado");
+let divAbertoFechado = document.querySelector(".divAbertoFechado")
+let iconAbertoFechado = document.querySelector(".iconAbertoFechado")
+
+if (horarioAtual >= 18 && horarioAtual < 23 && diaAtual != 1) {
+    abertoFechado.innerHTML = "Aberto"
+    divAbertoFechado.classList.add("bg-green-700")
+    iconAbertoFechado.classList.add("fa-solid", "fa-store")
+} else {
+    abertoFechado.innerHTML = "Fechado"
+    divAbertoFechado.classList.add("bg-red-700")
+    iconAbertoFechado.classList.add("fa-solid", "fa-store-slash")   
+}
 
 function comprar() {
     window.alert("Ainda não é possível comprar pelo site!\nUse apenas como cardápio")    
+}
+
+function enviarEmail() {
+    window.alert("Email enviado com sucesso!\nEm breve receberá informações e promoções")    
 }
